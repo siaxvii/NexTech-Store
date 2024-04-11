572584e56d55b2c348e146d01972a6c1fbf0c66f
Lasya Yakkala <yakkala.lasya@heb.com>
2024-04-11 03:35:39 -0500

    swaps shopping bag for shopping cart

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ShoppingBag } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -24,11 +24,11 @@ const NavbarActions = () => {
   return ( 
     <div className="ml-auto flex items-center gap-x-4">
       <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
-        <ShoppingBag
+        <ShoppingCart
           size={20}
           color="white"
         />
-        <span className="ml-2 text-sm font-medium text-white">
+        <span className="ml-2 text-sm font-medium text-white pl-1">
           {cart.items.length}
         </span>
       </Button>
