ddd69be2245b4ce13fb0d9010699e7dcbf2ab93d
Lasya Yakkala <yakkala.lasya@heb.com>
2024-04-11 03:35:08 -0500

    fixes navbar element spacing

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -24,7 +24,7 @@ const MainNav: React.FC<MainNavProps> = ({
 
   return (
     <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
+      className="mx-6 flex items-center space-x-2 lg:space-x-6"
     >
       {routes.map((route) => (
         <Link
