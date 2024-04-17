42fbaf1c49618a5f3b11ab715222b2bb71195c20
Lasya Yakkala <yakkala.lasya@heb.com>
2024-04-17 02:57:05 -0500

    fixes sign-in error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
-import { auth } from "@clerk/nextjs";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import ModalProvider from "@/providers/modal-provider"
 import Modal from "@/components/ui/modal";
 import { ClerkProvider } from "@clerk/nextjs";
 import ToastProvider from "@/providers/toast-provider"
-import { redirect } from "next/navigation";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -22,9 +20,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const { userId } = auth();
-  if (!userId){ redirect('/sign-in')}
   
   return (
     <ClerkProvider>
